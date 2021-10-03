453c9401ed8ae2a311a6c1820c5c4f72f9f485c7
Joel Smith <jsumnersmith@gmail.com>
2021-10-03 14:58:13 -0400

    Added basic links to site.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,35 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 // styles
 const pageStyles = {
   color: "#232129",
-  padding: 96,
+  margin: "0 auto",
+  maxWidth: 960,
+  width: "90%",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
   marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+  marginBottom: 20,
+  maxWidth: 800,
+  display: 'flex'
 }
-const headingAccentStyles = {
-  color: "#663399",
-}
+
 const paragraphStyles = {
-  marginBottom: 48,
+  marginBottom: 20,
 }
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+
 const listStyles = {
   marginBottom: 96,
-  paddingLeft: 0,
+  marginLeft: 0,
+  paddingLeft: 20,
 }
 const listItemStyles = {
   fontWeight: 300,
   fontSize: 24,
   maxWidth: 560,
-  marginBottom: 30,
+  marginBottom: 40,
 }
 
 const linkStyle = {
@@ -42,11 +39,6 @@ const linkStyle = {
   verticalAlign: "5%",
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
 
 const descriptionStyle = {
   color: "#232129",
@@ -54,12 +46,6 @@ const descriptionStyle = {
   marginTop: 10,
   marginBottom: 0,
   lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
 }
 
 const badgeStyle = {
@@ -78,51 +64,62 @@ const badgeStyle = {
   lineHeight: 1,
 }
 
+const columns = { 
+  display: 'flex',
+}
+
+const column = {
+  width: "45%",
+}
+
+
 // data
+const linksEnglish = [
+  {
+    text: "South Jersey Food Bank",
+    url: "https://foodbanksj.org/zip-code-locator/",
+    description:
+      "Use this website along with your zip code to find other locations where food is distributed on behalf of the Food Bank of South Jersey.",
+    color: "#443F8F",
+  },
+  {
+    text: "South Jersey Mutual Aid Network - Request Form",
+    url: "https://airtable.com/shrwkCwpZOqSkfbts",
+    description:
+      "Use this form to request food from the South Jersey Mutual Aid Network at large.",
+    color: "#443F8F",
+  },
+  {
+    text: "Free Baby Goods Exchange",
+    url: "https://docs.google.com/document/d/e/2PACX-1vS3BL5cNIkzZ7J6_56Mpq1QKphiiNBonJIcsQT1ISqYkX_mOZyYyAVmwtZ-aOReM3TWBe4J2Q9wiEBN/pub",
+    description:
+      "Oct 16: Pick up or drop off clothes for babies and kids, toys, books, baby gear, and maternity clothes.",
+    color: "#443F8F",
+  }
+]
+
 const links = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
+    text: "Banco de Alimentos de South Jersey",
+    url: "https://foodbanksj.org/zip-code-locator/",
     description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+      "Utilice este sitio con su código postal para encontrar otra locacciones donde se distribuye comida con el Banco de Alimentos de South Jersey.",
+    color: "#443F8F",
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
+    text: "South Jersey Mutual Aid Network - Forma de Solicitar",
+    url: "https://airtable.com/shrwkCwpZOqSkfbts",
     description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+      "Utilice esta forma para solicitar alimentos de la South Jersey Mutual Aid Network en general.",
+    color: "#443F8F",
   },
   {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
+    text: "Intercambio Gratis de Artículos de Bebés",
+    url: "https://docs.google.com/document/d/e/2PACX-1vS3BL5cNIkzZ7J6_56Mpq1QKphiiNBonJIcsQT1ISqYkX_mOZyYyAVmwtZ-aOReM3TWBe4J2Q9wiEBN/pub",
     description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
+      "Oct. 16: Recoja o deje ropa para bebés y niños, juguetes, libros, artículos para bebés y ropa de maternidad.",
+    color: "#443F8F",
+  }
 ]
 
 // markup
@@ -130,53 +127,65 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
+      <header style={headingStyles}>
+        <StaticImage src="../images/logo.jpg" style={{width: 100, height: 'auto', marginRight: 20}}/> 
+        <div style={{grow: 1}}>
+          <h1 style={{margin: "5px 0 10px"}}>Más Recursos / Additional Resources</h1>
+          <h2 style={{color: "#443F8F", margin: 0}}>South Jersey Mutual Aid Network - Pennsuaken</h2>
+        </div>
+      </header>
+      <div style={columns}>
+        <div style={column}>
+          <p style={paragraphStyles}>
+            Si buscas más recursos sobre alimentos, aquí son unos sitios de web que tienen mas infomaccíon. 
+          </p>
+          <ul style={listStyles}>
+            {links.map(link => (
+              <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+                <span>
+                  <a
+                    style={linkStyle}
+                    href={`${link.url}`}
+                  >
+                    {link.text}
+                  </a>
+                  {link.badge && (
+                    <span style={badgeStyle} aria-label="New Badge">
+                      NEW!
+                    </span>
+                  )}
+                  <p style={descriptionStyle}>{link.description}</p>
                 </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div style={{...column, marginLeft: '10%'}}>
+          <p style={paragraphStyles}>
+            If you are looking for additional food resources, here are a few websites where you can find more information.
+          </p>
+          <ul style={listStyles}>
+            {linksEnglish.map(link => (
+              <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+                <span>
+                  <a
+                    style={linkStyle}
+                    href={`${link.url}`}
+                  >
+                    {link.text}
+                  </a>
+                  {link.badge && (
+                    <span style={badgeStyle} aria-label="New Badge">
+                      NEW!
+                    </span>
+                  )}
+                  <p style={descriptionStyle}>{link.description}</p>
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </main>
   )
 }
